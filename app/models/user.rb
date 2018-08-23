a87600d983b4ac95a9037a019456b257e24fa593
ashwink2718 <ashwink2718@gmail.com>
2018-08-23 22:01:51 +0200

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  
+  validates :first_name, presence: true, length: { minimum:3, maximum:50 }
+  validates :last_name, presence: true, length: { minimum:3, maximum:50 }
+  validates :email, presence: true, length: { minimum:3, maximum:50 }
 end
+
+
